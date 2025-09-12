37fc4ae2b27e52153e362e5e86f91c024a789816
Emi Covreag <emi.covreag@gmail.com>
2025-09-13 02:30:10 +0300

    Initial setup

---
+++ src/main/java/org/emi/spring_ecommerce_be/SpringEcommerceBeApplication.java
@@ -0,0 +1,13 @@
+package org.emi.spring_ecommerce_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEcommerceBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEcommerceBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/emi/spring_ecommerce_be/SpringEcommerceBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.emi.spring_ecommerce_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEcommerceBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
